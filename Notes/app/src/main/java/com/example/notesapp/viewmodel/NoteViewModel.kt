454a3108e831775ce5e3d4214cac2d4994be4ddf
newerlyn <mr.hiket@gmail.com>
2024-06-25 14:18:46 +0500

    NoteViewModel.kt

---
+++ Notes/app/src/main/java/com/example/notesapp/viewmodel/NoteViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.notesapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.notesapp.database.NoteDatabase
+import com.example.notesapp.model.Note
+import com.example.notesapp.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: NoteRepository
+    val allNotes: LiveData<List<Note>>
+
+    init {
+        val noteDao = NoteDatabase.getDatabase(application).noteDao()
+        repository = NoteRepository(noteDao)
+        allNotes = repository.allNotes
+    }
+
+    fun insert(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+
+    fun update(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+
+    fun delete(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+}
